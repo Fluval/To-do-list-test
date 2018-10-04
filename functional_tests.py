1d7ac2f797eb016857b7ec13d5cc69185e809afa
omen <vadimpolubedov@gmail.com>
2018-10-04 22:44:29 +0300

    функциональный тест проверяет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     '''тест нового посетителя'''
@@ -13,6 +15,18 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://127.0.0.1:8000')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text =='1: Купить павлиньи перья' for row in rows))        
         self.fail('Закончить тест')
 
 if __name__ == '__main__':
