4626c8a25c5f321fb604d9b35691ce16ce9a9aaa
omen <vadimpolubedov@gmail.com>
2018-10-15 23:46:52 +0300

    добавлена тест нескольких посетителей

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,6 +10,7 @@ from selenium.common.exceptions import WebDriverException
 
 MAX_WAIT = 10
 
+
 class NewVisitorTest(LiveServerTestCase):
     """тест нового посетителя"""
 
@@ -51,3 +52,42 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_list_table('1 Купить павлиньи перья')
         self.wait_for_row_list_table('2 Сделать мушку из павлиньих перьев')
         self.fail(u'Закончить тест')
+
+    def test_can_start_a_list_for_one_user(self):
+        """тест: можно начать список для одного пользователя"""
+        self.wait_for_row_list_table('2 Сделать мушку из павлиньих перьев')
+        self.wait_for_row_list_table('1 Купить павлиньи перья')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        """тест: многочисленные пользователи могут начать списки по разным url"""
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_list_table('1 Купить павлиньи перья')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        # новый сеанс браузера
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('bodu').text
+        self.assertNotIn("Купить павлиньи перья", page_text)
+        self.assertNotIn("Сделать мушку из павлиньих перьев", page_text)
+
+        # второй пользователь вводит новый список
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Купить молоко')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_list_table('1 Купить молоко')
+
+        # второй пользователь получает уникальный адрес
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # проверка следов первого пользователя
+        page_text = self.browser.find_element_by_tag_name('bodu').text
+        self.assertNotIn("Купить павлиньи перья", page_text)
+        self.assertNotIn("Сделать мушку из павлиньих перьев", page_text)
